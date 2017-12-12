08bfdf3edb9ee1a8e559a51c470ab0d0240e169e
csslick <digitone@naver.com>
2017-12-13 06:32:58 +0900

    mongodb uri updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;	// ES6(node.js) Promise 사용(콜백 대체)
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+// 실재 서버와 로컬 서버 주소 동시 지정(팁)
+var MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds127842.mlab.com:27842/csslick_db';
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 
