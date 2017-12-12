b4656970a3d9f63b1202765f59e614afd2629449
csslick <digitone@naver.com>
2017-12-13 07:08:38 +0900

    update moongoose path in server.js

--- server/server3.js
+++ server/server3.js
@@ -3,11 +3,11 @@ var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
 // 몽구스
-var {mongoose} = require(__dirname + '/db/mongoose');
+var {mongoose} = require('./../db/mongoose');
 
 // 스키마 모델
-var {Todo} = require(__dirname + '/models/todo');
-var {User} = require(__dirname + '/models/user');
+var {Todo} = require('./../models/todo');
+var {User} = require('./../models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
